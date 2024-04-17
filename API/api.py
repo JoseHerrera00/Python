d754b17e486a9f5acccbbd320d687cfc61c5a0cd
Tu Nombre <jh7641013@gmail.com>
2024-04-17 18:52:35 -0500

    Pruebas de API

---
+++ API/api.py
@@ -0,0 +1,6 @@
+import requests
+url = "https://www.google.com.mx/"
+reponse = requests.get(url)
+
+print (reponse)
+
---
+++ Variables_y_tipos/Variables.py
@@ -0,0 +1,8 @@
+#En python no es necesario declarar el tipo de variable que uno requiere usar,
+ #Pyhon define el tipo de valor por defecto
+
+i=23
+Nombre = "jose"
+print(Nombre+" tine "+str(i)) #Para concatenar un valor entero con un String, se debe agregar (str)
+
+                                
