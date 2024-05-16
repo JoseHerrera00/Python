54302cca3457be63a430d7fd987f9fb86ff3de6a
Tu Nombre <jh7641013@gmail.com>
2024-05-16 15:40:43 -0500

    Cadena

--- API/tipodatos.py
+++ API/tipodatos.py
@@ -1,5 +1,9 @@
 ## estos son los tipos de datos mas importantes.
 nada = None  ## Esto es un tipo de dato Nada
-cadena = "esto es una cadena"  #Tipo de dato cadena
-
-print (type(cadena))
+cadena = "esto es una cadena"  #Tipo de dato cadenaen
+entero = 99
+flotante = 4.2
+booleano = False
+tuplanocambia = ("mastaer en", "Python")
+print(tuplanocambia)
+print (type(tuplanocambia ))
