11083dbc960b0218c888b23612017c9735177fb7
Tu Nombre <jh7641013@gmail.com>
2024-05-17 11:26:38 -0500

    Nuevos campos

---
+++ Master-Python/Variables_y_tipos/cambiardatos.py
@@ -0,0 +1,11 @@
+texto = "Hola yo tengo \"este\" numero" ## De esta forma podemos imprimir el valor en comillas
+numero = str(77)  ## indicados el tipo de valor al que queremos que se invierta
+print (texto + "" + numero)
+print(type(texto))
+
+print (numero)
+print(type(numero))
+
+numero2 = float(555)
+print (numero2)
+print(type(numero2))
--- Master-Python/Variables_y_tipos/tipodatos.py
+++ Master-Python/Variables_y_tipos/tipodatos.py
@@ -1,4 +1,4 @@
-## estos son los tipos de datos mas importantes.
+## estos son los tipos de datos mas importantes.------------------------------------------------------------------------------
 nada = None  ## Esto es un tipo de dato Nada
 cadena = "esto es una cadena"  #Tipo de dato cadenaen
 entero = 99
