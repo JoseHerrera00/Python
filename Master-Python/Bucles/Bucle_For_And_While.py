2191cc7d28c5abb409d7c92803c487201029f39e
Tu Nombre <jh7641013@gmail.com>
2024-06-25 10:54:54 -0500

    Bucle While

--- Master-Python/Bucles/Bucle_For_And_While.py
+++ Master-Python/Bucles/Bucle_For_And_While.py
@@ -1,10 +1,30 @@
 
-contador =0
+"""contador =0
 resultado =0
 for contador in range(0,5):
     print("Voy por el valor"+str(contador))
     resultado+=contador
 
 print(f"El valor de la suma es "+ str(resultado))
+"""
 
+"""
+contadorw=1 
+while contadorw <=10:
+    print(f"Vamos en el While por el valor {contadorw}")
+    contadorw+=1
+
+"""
+
+
+numuser= int(input("Ingrese el numero: "))
+
+if numuser < 1:
+    numuser =1
+
+conta=1
+
+while conta <=10:
+    print(f"{numuser} * {conta} = {numuser*conta}")
+    conta+=1
 
